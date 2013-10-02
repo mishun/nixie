cad61a23077c8831ce2a5c5fa27d6ce28bb28019
Alexander Mishunin <alexander.mishunin@gmail.com>
2013-10-02 18:32:22 +0400

    Sensor buttons implementation started + date and time routines

--- firmware/nixie.cpp
+++ firmware/nixie.cpp
@@ -351,6 +351,147 @@ ISR(INT0_vect)
 }
 
 
+class Sensor
+{
+private:
+	static Sensor sensor;
+
+private:
+	Sensor()
+	{
+		// ADC result left adjust
+		//ADMUX = (1 << ADLAR);
+		//ADCSRA = (1 << ADEN) | (1 << ADIE) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+
+		// Timer 1
+		//TCNT1 = 0x0000;
+		//OCR1A = 0x8000;
+		//OCR1B = 0x0080;
+		//TCCR1A &= ~(1 << WGM10) & ~(1 << WGM11);
+		//TCCR1B = (TCCR1B & ~(1 << CS10) & ~(1 << CS11) & ~(1 << CS12) & ~(1 << WGM13)) | (1 << CS10) | (1 << WGM12);
+		//TIMSK = (1 << OCIE1A) | (1 << OCIE1B);
+	}
+
+public:
+	static void timerAInterrupt()
+	{
+		DDRC &= 0xF0;
+		PORTC &= 0xF0;
+	}
+
+	static void timerBInterrupt()
+	{
+		ADCSRA |= (1 << ADSC);
+	}
+
+	static void adcInterrupt()
+	{
+		/*
+		DDRC |= 0x0F;
+		PORTC &= 0xF0;
+		unsigned char value = ADCH;
+
+		if(clock_flags & POWER_DOWN)
+			return;
+
+		const unsigned char SIZE = 4;
+		const unsigned char button_id = 0;
+		static unsigned short sum = 0;
+		static unsigned char head = 0;
+		static unsigned char wait = 2;
+		static unsigned char buffer[1 << SIZE];
+		static unsigned char mean[1 << SIZE];
+		static bool button = false;
+
+		if(wait < 2) sum -= buffer[head];
+		buffer[head] = value;
+		sum += value;
+		head = (head + 1) & ((1 << SIZE) - 1);
+		mean[head] = (unsigned char)(sum >> SIZE);
+		if(head == 0 && wait > 0)
+			wait--;
+
+		if(wait == 0)
+		{
+			unsigned char cur = mean[head];
+			unsigned char prev = mean[(head + 1) & ((1 << SIZE) - 1)];
+			if(button)
+			{
+				if(cur - (cur >> 3) > prev)
+					button = false;
+			}
+			else
+			{
+				if(prev - (prev >> 3) > cur)
+					button = true;
+			}
+		}
+
+		if(button)
+			clock_flags |= (1 << button_id);
+		else
+			clock_flags &= ~(1 << button_id);
+
+		//void setADCInput(unsigned char id)
+		//{
+		//	ADMUX = (ADMUX & ~0x03) | (id & 0x03);
+		//}
+		 */
+	}
+};
+
+Sensor Sensor::sensor;
+
+ISR(TIMER1_COMPA_vect)
+{
+	Sensor::timerAInterrupt();
+}
+
+ISR(TIMER1_COMPB_vect)
+{
+	Sensor::timerBInterrupt();
+}
+
+ISR(ADC_vect)
+{
+	Sensor::adcInterrupt();
+}
+
+
+class DateTime
+{
+public:
+	static unsigned char isLeapYear(unsigned char year)
+	{
+		if(year % 4 == 0)
+		{
+			if(year % 100 == 0)
+			{
+				if(year % 400 == 0)
+					return 1;
+				return 0;
+			}
+			return 1;
+		}
+		return 0;
+	}
+
+	static unsigned char daysInMonth(unsigned char month, unsigned char year)
+	{
+		const static unsigned char days[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+		return days[month - 1] + (month == 2 ? isLeapYear(year) : 0);
+	}
+	
+	static unsigned char getDayOfWeek(unsigned char day, unsigned char month, unsigned char year)
+	{
+		unsigned short a = (14 - month) / 12;
+		unsigned short y = 2000 + year - a;
+		unsigned short m = month + 12 * a - 2;
+		return (7000 + ((unsigned short)day + y + y / 4 - y / 100 + y / 400 + (31 * m) / 12)) % 7;
+	}
+};
+
+
 int main()
 {
 	// Enable watchdog
@@ -376,240 +517,5 @@ int main()
 		}
 	}
 
-	// Analog comparator
-	//ACSR = (ACSR & ~(1 << ACIS0) & ~(1 << ACIS1)) | (1 << ACIE);
-
-	// ADC result left adjust
-	//ADMUX = (1 << ADLAR);
-	//ADCSRA = (1 << ADEN) | (1 << ADIE) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
-
-	// Timer
-	//TCNT1 = 0x0000;
-	//OCR1A = 0x8000;
-	//OCR1B = 0x0080;
-	//TCCR1A &= ~(1 << WGM10) & ~(1 << WGM11);
-	//TCCR1B = (TCCR1B & ~(1 << CS10) & ~(1 << CS11) & ~(1 << CS12) & ~(1 << WGM13)) | (1 << CS10) | (1 << WGM12);
-	//TIMSK = (1 << OCIE1A) | (1 << OCIE1B);
-
-/*	for(;;)
-	{
-		if(clock_flags & POWER_DOWN)
-		{
-			display.shutdown();
-			continue;
-		}
-
-		if(clock_flags & BUTTON_0)
-		{
-			clock_flags |= SHOW_DATE;
-			updateTime();
-			delay_counter = 6;
-			continue;
-		}
-
-		if(clock_flags & TIME_CHANGE)
-		{
-			updateTime();
-			clock_flags &= ~TIME_CHANGE;
-		}
-	}*/
-
 	return 0;
 }
-
-/*
-volatile unsigned char clock_flags = 0;
-const unsigned char BUTTON_0    = 0x01;
-const unsigned char BUTTON_1    = 0x02;
-const unsigned char TIME_CHANGE = 0x10;
-const unsigned char SHOW_DATE   = 0x20;
-const unsigned char POWER_DOWN  = 0x80;
-unsigned char delay_counter;
-
-
-ISR(TIMER1_COMPA_vect)
-{
-	DDRC &= 0xF0;
-	PORTC &= 0xF0;
-}
-
-ISR(TIMER1_COMPB_vect)
-{
-	ADCSRA |= (1 << ADSC);
-}
-
-ISR(ADC_vect)
-{
-	DDRC |= 0x0F;
-	PORTC &= 0xF0;
-	unsigned char value = ADCH;
-
-	if(clock_flags & POWER_DOWN)
-		return;
-
-	const unsigned char SIZE = 4;
-	const unsigned char button_id = 0;
-	static unsigned short sum = 0;
-	static unsigned char head = 0;
-	static unsigned char wait = 2;
-	static unsigned char buffer[1 << SIZE];
-	static unsigned char mean[1 << SIZE];
-	static bool button = false;
-
-	if(wait < 2) sum -= buffer[head];
-	buffer[head] = value;
-	sum += value;
-	head = (head + 1) & ((1 << SIZE) - 1);
-	mean[head] = (unsigned char)(sum >> SIZE);
-	if(head == 0 && wait > 0)
-		wait--;
-
-	if(wait == 0)
-	{
-		unsigned char cur = mean[head];
-		unsigned char prev = mean[(head + 1) & ((1 << SIZE) - 1)];
-		if(button)
-		{
-			if(cur - (cur >> 3) > prev)
-				button = false;
-		}
-		else
-		{
-			if(prev - (prev >> 3) > cur)
-				button = true;
-		}
-	}
-
-	if(button)
-		clock_flags |= (1 << button_id);
-	else
-		clock_flags &= ~(1 << button_id);
-
-	//void setADCInput(unsigned char id)
-	//{
-	//	ADMUX = (ADMUX & ~0x03) | (id & 0x03);
-	//}
-}
-
-ISR(ANA_COMP_vect)
-{
-	if(ACSR & (1 << ACO))
-		clock_flags |= POWER_DOWN;
-	else
-		clock_flags &= ~POWER_DOWN;
-}
-
-unsigned char isLeap(unsigned char year)
-{
-	if(year % 4 == 0)
-	{
-		if(year % 100 == 0)
-		{
-			if(year % 400 == 0) return 1;
-			return 0;
-		}
-		return 1;
-	}
-	return 0;
-}
-
-unsigned char getDay(unsigned char day, unsigned char month, unsigned char year)
-{
-	unsigned short a = (14 - month) / 12;
-	unsigned short y = 2000 + year - a;
-	unsigned short m = month + 12 * a - 2;
-	return (7000 + ((unsigned short)day + y + y / 4 - y / 100 + y / 400 + (31 * m) / 12)) % 7;
-}
-
-unsigned char daysInMonth(unsigned char month, unsigned char year)
-{
-	const static unsigned char month_length[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	if(month == 2)
-		return month_length[1] + isLeap(year);
-	return month_length[month - 1];
-}
-
-bool isSummerTime(unsigned char hours, unsigned char day, unsigned char month, unsigned char year)
-{
-	if(month > 3 && month < 10)
-		return true;
-	if(month < 3 || month > 10)
-		return false;
-
-	unsigned char last_sunday = 25;
-	unsigned char d = getDay(last_sunday, month, year);
-	if(d != 0)
-		last_sunday += 7 - d;
-
-	if(month == 3)
-	{
-		if(day > last_sunday)
-			return true;
-		if(day == last_sunday)
-			return hours >= 2;
-		return false;
-	}
-
-	if(day < last_sunday)
-		return true;
-	if(day == last_sunday)
-		return hours < 2;
-	return false;
-}
-
-unsigned char incBCD(unsigned char val)
-{
-	if((val & 0x0F) == 9)
-		return (val & 0xF0) + 0x10;
-	return val + 0x01;
-}
-
-void updateTime()
-{
-	rtc.read();
-
-	unsigned char time[8];
-	time[0] = rtc.seconds;
-	time[1] = rtc.minutes;
-	time[2] = rtc.hours;
-	time[3] = rtc.date;
-	time[4] = rtc.month;
-	time[5] = rtc.year;
-
-	if(isSummerTime(time[2], time[3], time[4], time[5]))
-	{
-		time[2] = incBCD(time[2]);
-		if(time[2] >= 0x24)
-		{
-			time[2] = 0x00;
-			time[3] = incBCD(time[3]);
-			if(time[3] > daysInMonth(time[4], time[5]))
-			{
-				time[3] = 0x01;
-				time[4] = incBCD(time[4]);
-				if(time[4] > 12)
-				{
-					time[4] = 0x01;
-					time[5] = incBCD(time[5]);
-				}
-			}
-		}
-	}
-
-	if(clock_flags & SHOW_DATE)
-	{
-		display.data[0] = time[5];
-		display.data[1] = time[4];
-		display.data[2] = time[3];
-		if(--delay_counter == 0)
-			clock_flags &= ~SHOW_DATE;
-	}
-	else
-	{
-		display.data[0] = time[0];
-		display.data[1] = time[1];
-		display.data[2] = time[2];
-	}
-	display.update();
-}
-*/
